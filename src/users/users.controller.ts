6992f9199521c15f2efa969027d117d3a32fb37b
youssef ezzat <ye.fci2020@gmail.com>
2022-11-01 01:53:27 +0200

    Remove unused endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -6,13 +6,13 @@ import { CreateUserDto } from './dto/create-user.dto';
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
-  }
+  // @Post()
+  // create(@Body() createUserDto: CreateUserDto) {
+  //   return this.usersService.create(createUserDto);
+  // }
 
-  @Get()
-  findAll() {
-    return this.usersService.findAll();
-  }
+  // @Get()
+  // findAll() {
+  //   return this.usersService.findAll();
+  // }
 }

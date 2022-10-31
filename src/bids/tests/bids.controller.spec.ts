1278735637a23bffa1fb7b1a1a8af05a2d06b769
youssef ezzat <ye.fci2020@gmail.com>
2022-11-01 00:45:28 +0200

    Add unit tests to pets service

--- src/bids/tests/bids.controller.spec.ts
+++ src/bids/tests/bids.controller.spec.ts
@@ -4,11 +4,21 @@ import { BidsService } from '../bids.service';
 
 describe('BidsController', () => {
   let controller: BidsController;
+  let bidsService = {
+    create: jest.fn(),
+    findAll: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [BidsController],
-      providers: [BidsService],
+      providers: [
+        BidsService,
+        {
+          provide: BidsService,
+          useValue: bidsService,
+        },
+      ],
     }).compile();
 
     controller = module.get<BidsController>(BidsController);
--- src/pets/tests/pets.service.spec.ts
+++ src/pets/tests/pets.service.spec.ts
@@ -1,18 +1,51 @@
+import { getModelToken } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Model } from 'mongoose';
 import { PetsService } from '../pets.service';
+import { Pet } from '../schemas/pet.schema';
 
 describe('PetsService', () => {
   let service: PetsService;
-
+  let petRepository: any;
+  const petModelToken = getModelToken('Pet');
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PetsService],
+      providers: [
+        PetsService,
+        {
+          provide: petModelToken,
+          useValue: {
+            create: jest.fn(),
+            findOneByOwner: jest.fn(),
+            save: jest.fn(),
+            findOne: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<PetsService>(PetsService);
+    petRepository = module.get<Model<Pet>>(getModelToken('Pet'));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('Find Bids', () => {
+    it('should find all bids by petId', async () => {
+      const pet = {
+        _id: 'fakeId',
+        name: 'cat',
+        status: 'available',
+        ownerId: 'fakeId',
+      };
+      const ownerId = 'fakeId';
+      const _id = 'fakeId';
+      jest.spyOn(petRepository, 'findOne').mockResolvedValueOnce(pet);
+      const data = await service.findOneByOwner(ownerId, _id);
+      expect(petRepository.findOne).toHaveBeenCalledWith({ ownerId, _id });
+      expect(data).toEqual(pet);
+    });
   });
 });
